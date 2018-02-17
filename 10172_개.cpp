889ccaec966cc5a779cb74f0f1e565a9665d8b8b
min-ki <min_2271@naver.com>
2018-02-17 04:42:05 +0000

    10172번 개 by c++

---
+++ 10172_개.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+	printf("%s\n", "|\\_/|");
+	printf("%s\n", "|q p|   /}");
+	printf("%s\n", "( 0 )\"\"\"\\");
+	printf("%s\n", "|\"^\"`    |");
+	printf("%s",   "||_/=\\\\__|");
+
+	return 0;
+}
